8f9f87358831961af80d5e6dee4c579eaca7a12f
Matias Salaberry <matiasfederico.salaberrycapdevila@alumnos.uai.edu.ar>
2024-11-13 21:24:50 -0300

    EntityFramework y DB using Classes
    Se crearon las entidades para el uso de EntityFramework y se configuro el AppDbContext para crear la base de datos desde la terminal del IDE

---
+++ DAL/Migrations/20241114002226_InitialCreate.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    plate = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    brand = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    pricePerDay = table.Column<double>(type: "float", nullable: false),
+                    year = table.Column<int>(type: "int", nullable: false),
+                    kilometers = table.Column<int>(type: "int", nullable: false),
+                    color = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    details = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.plate);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Dni = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LicenseValidDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    QuantityOfRents = table.Column<int>(type: "int", nullable: false),
+                    BlackList = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Dni);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Insurances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TypeOfInsurance = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Insurances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdClient = table.Column<int>(type: "int", nullable: false),
+                    Plate = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    IdInsurance = table.Column<int>(type: "int", nullable: false),
+                    RentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ReturnDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FinalPrice = table.Column<double>(type: "float", nullable: false),
+                    CreditCardNum = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rents_Cars_Plate",
+                        column: x => x.Plate,
+                        principalTable: "Cars",
+                        principalColumn: "plate",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rents_Clients_IdClient",
+                        column: x => x.IdClient,
+                        principalTable: "Clients",
+                        principalColumn: "Dni",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rents_Insurances_IdInsurance",
+                        column: x => x.IdInsurance,
+                        principalTable: "Insurances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rents_IdClient",
+                table: "Rents",
+                column: "IdClient");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rents_IdInsurance",
+                table: "Rents",
+                column: "IdInsurance");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rents_Plate",
+                table: "Rents",
+                column: "Plate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rents");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Insurances");
+        }
+    }
+}
--- UI/obj/Debug/net8.0-windows/UI.AssemblyInfo.cs
+++ UI/obj/Debug/net8.0-windows/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63cc1e85e8f3dfe3d77d6f36dbce84beb602204e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ffc769801af37a0f747719bc8d4b8067fa87fbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
