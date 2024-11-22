f9e3437828721ea3a5c4c0fd8ee3e60fdb882432
Matias Salaberry <matiasfederico.salaberrycapdevila@alumnos.uai.edu.ar>
2024-11-21 21:33:05 -0300

    CarData Develop
    Added functions:
    ListAllCars()
    
    GetByPlate()
    
    AddCar()
    
    ModCar()
    
    DeleteCar()

--- DAL/CarData.cs
+++ DAL/CarData.cs
@@ -1,4 +1,6 @@
 ﻿using Entity;
+using Entity.model;
+using Mapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +11,27 @@ namespace DAL
 {
     public class CarData
     {
-
-        public void ListAllCars() 
+        private static AppDbContext getAppDbContext()
+        {
+            AppDbContextFactory appDbContextFactory = new AppDbContextFactory();
+            AppDbContext appDbConext = appDbContextFactory.CreateDbContext(null);
+            return appDbConext;
+        }
+        public List<CarEntity> ListAllCars() 
         {
 			try
 			{
 				List<CarEntity> carList = new List<CarEntity>();
 
+                using(AppDbContext appDbContext = getAppDbContext())
+                {
+                    List<Car> carsDb = appDbContext.Cars.ToList();
+                    foreach (Car car in carsDb)
+                    {
+                        carList.Add(CarMapper.Map(car));
+                    }
+                    return carList;
+                }
 
             }
 			catch (Exception)
@@ -23,6 +39,81 @@ namespace DAL
 
 				throw;
 			}
+        }
+
+        public CarEntity GetCarByPlate(string plateToFind)
+        {
+            try
+            {
+                using (AppDbContext appDbContext = getAppDbContext())
+                {
+                    return CarMapper.Map(appDbContext.Cars.Find(plateToFind));
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void AddCar(CarEntity car)
+        {
+            try
+            {
+                using (AppDbContext appDbContext = getAppDbContext())
+                {
+                    appDbContext.Cars.Add(CarMapper.Map(car));
+                    appDbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void ModCar(CarEntity carMod)
+        {
+            try
+            {
+                using(AppDbContext appDbContext = getAppDbContext())
+                {
+                    Car carToMod = appDbContext.Cars.Find(carMod.Plate);
+                    carToMod.plate = carMod.Plate;
+                    carToMod.brand = carMod.Brand;
+                    carToMod.model = carMod.Model;
+                    carToMod.pricePerDay = carMod.PricePerDay;
+                    carToMod.year = carMod.Year;
+                    carToMod.kilometers = carMod.Kilometers;
+                    carToMod.color = carMod.Color;
+                    carToMod.details = carMod.Details;
+                    appDbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void DeleteCar(string plateToDelete)
+        {
+            try
+            {
+                using (AppDbContext appDbContext = getAppDbContext())
+                {
+                    appDbContext.Cars.Remove(appDbContext.Cars.Find(plateToDelete));
+                    appDbContext.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
     }
